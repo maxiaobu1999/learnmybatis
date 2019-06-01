106d3955c8e9f1a627abd77b4f6894f87fb81594
maxiaobu1999 <maqinglong1999@163.com>
2019-06-01 23:31:05 +0800

    Initial commit

---
+++ src/main/java/com/norman/dao/IUserDao.java
@@ -0,0 +1,23 @@
+package com.norman.dao;
+
+
+import com.norman.domain.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ *
+ * 用户的持久层接口
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有操作
+     * @return
+     */
+    @Select("select * from user")
+    List<User> findAll();
+}
