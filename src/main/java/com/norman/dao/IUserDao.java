36860cf36ad7b483b2359a87c03d9104f586c828
maxiaobu1999 <maqinglong1999@163.com>
2019-06-04 20:07:43 +0800

    【功能】使用注解curd

--- src/main/java/com/norman/dao/IUserDao.java
+++ src/main/java/com/norman/dao/IUserDao.java
@@ -1,68 +1,70 @@
 package com.norman.dao;
 
-
-import com.norman.domain.QueryVo;
 import com.norman.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 /**
  * @author 黑马程序员
  * @Company http://www.ithiema.com
- *
- * 用户的持久层接口
+ * 在mybatis中针对,CRUD一共有四个注解
+ *  @Select @Insert @Update @Delete
  */
 public interface IUserDao {
 
     /**
-     * 查询所有用户，同时获取到用户下所有账户的信息
+     * 查询所有用户
      * @return
      */
-//    @Select("select * from user")
+    @Select("select * from user")
     List<User> findAll();
 
     /**
      * 保存用户
      * @param user
      */
+    @Insert("insert into user(username,address,sex,birthday)values(#{username},#{address},#{sex},#{birthday})")
     void saveUser(User user);
 
     /**
      * 更新用户
      * @param user
      */
+    @Update("update user set username=#{username},sex=#{sex},birthday=#{birthday},address=#{address} where id=#{id}")
     void updateUser(User user);
+
     /**
-     * 根据Id删除用户
+     * 删除用户
      * @param userId
      */
+    @Delete("delete from user where id=#{id} ")
     void deleteUser(Integer userId);
+
     /**
-     * 根据id查询用户信息
+     * 根据id查询用户
      * @param userId
      * @return
      */
+    @Select("select * from user  where id=#{id} ")
     User findById(Integer userId);
 
     /**
-     * 根据名称模糊查询用户信息
+     * 根据用户名称模糊查询
      * @param username
      * @return
      */
-    List<User> findByName(String username);
+//    @Select("select * from user where username like #{username} ")
+    @Select("select * from user where username like '%${value}%' ")
+    List<User> findUserByName(String username);
 
     /**
-     * 查询总用户数
+     * 查询总用户数量
      * @return
      */
-    int findTotal();
-
-    /**
-     * 根据queryVo中的条件查询用户
-     * @param vo
-     * @return
-     */
-    List<User> findUserByVo(QueryVo vo);
+    @Select("select count(*) from user ")
+    int findTotalUser();
 }
-
--- src/main/java/com/norman/domain/User.java
+++ src/main/java/com/norman/domain/User.java
@@ -2,62 +2,33 @@ package com.norman.domain;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 /**
  * @author 黑马程序员
  * @Company http://www.ithiema.com
  */
-public class User implements Serializable {
+public class User implements Serializable{
 
-
-    private Integer userId;
-    private String userName;
-    private Date birthday;
-    private String sex;
+    private Integer id;
+    private String username;
     private String address;
+    private String sex;
+    private Date birthday;
 
-    public List<Account> getAccounts() {
-        return accounts;
+    public Integer getId() {
+        return id;
     }
 
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    //一对多关系映射：主表实体应该包含从表实体的集合引用
-    private List<Account> accounts;
-
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public void setUserName(String username) {
-        this.userName = username;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getAddress() {
@@ -68,14 +39,30 @@ public class User implements Serializable {
         this.address = address;
     }
 
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
     @Override
     public String toString() {
         return "User{" +
-                "id=" + userId +
-                ", username='" + userName + '\'' +
-                ", birthday=" + birthday +
-                ", sex='" + sex + '\'' +
+                "id=" + id +
+                ", username='" + username + '\'' +
                 ", address='" + address + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
                 '}';
     }
 }
